c9271ab1c0f214066af17d52c02fa7e8ea241cfe
Aboubakar Yatera <yatera.abou@gmail.com>
2023-12-21 16:06:12 +0100

    Commit : Frontend - TripsScreen ready + UpdateTrip ready

--- screens/TripPlanScreen.js
+++ screens/TripPlanScreen.js
@@ -67,7 +67,7 @@ export default function TripPlanScreen({ navigation }) {
   ];
   const [spinner, setSpinner] = useState(false);
   let size = [2, 4];
-  const PLACES_API_KEY = "AIzaSyDIHWBTXDGk6XeIiwAxnIX2tXN44o1nE7M";
+  const PLACES_API_KEY = "**************";
 
   useEffect(() => {
     const tempArray = [];
